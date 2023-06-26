45bca729b20b07500b23a6ba3efca2239d3b3ea9
Object-ions <moshikoatia@gmail.com>
2023-06-26 15:13:34 -0700

    finilize the Gifhy app.
    Co-Authored-by: Jason Church elijahchurch@gmail.com

--- src/js/index.js
+++ src/js/index.js
@@ -4,23 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Business Logic
 
-function getWeather(city) {
+function getWeather(searchInput) {
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
-
+    const url = `https://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=${process.env.API_KEY}`;
     request.addEventListener("loadend", function () {
         const response = JSON.parse(this.responseText);
         if (this.status === 200) {
-            printElements(response, city);
+            printElements(response, searchInput);
         } else {
-            printError(this, response, city);
+            printError(this, response, searchInput);
         }
     });
-
     request.open("GET", url, true);
     request.send();
     request.addEventListener("readystatechange", function () {
-        console.log(this.readyState);
     });
 }
 
@@ -30,16 +27,32 @@ function printError(request, apiResponse, city) {
     document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}:  ${request.status} ${request.statusText}: ${apiResponse.message}`;
 }
 
-function printElements(apiResponse, city) {
-    document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-    The temperature in Farenheight is ${apiResponse.main.temp} degrees. A brief description for today's weather in ${city} is ${apiResponse.weather[0].description}. Sunrise today is: ${new Date(apiResponse.sys.sunrise * 1000)}`;
+function printElements(apiResponse) {
+    // let img1 = document.querySelector('#img1');
+    // let img2 = document.querySelector('#img2');
+    // let img3 = document.querySelector('#img3');
+    // let img4 = document.querySelector('#img4');
+
+    let arrayImg = [];
+
+    for (let index = 1; index < 5; index++) {
+        let img = document.querySelector(`#img${index}`);
+        arrayImg.push(img);
+    }
+
+    arrayImg.forEach(function (img) {
+        let i = Math.floor(Math.random() * (50 - 0 + 1) + 0);
+        let url = apiResponse.data[i].images.downsized.url;
+        img.src = url;
+    });
 }
 
 function handleFormSubmission(event) {
+
+    const searchInput = document.querySelector('#search').value;
+    document.querySelector('#search').value = null;
     event.preventDefault();
-    const city = document.querySelector('#location').value;
-    document.querySelector('#location').value = null;
-    getWeather(city);
+    getWeather(searchInput);
 }
 
 window.addEventListener("load", function () {
